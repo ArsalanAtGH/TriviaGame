f59fa8fb52e66f453030d77de002bb9ffb64ed9e
My Git Arsalan <asadri62@gmail.com>
2018-08-24 15:05:48 -0700

    ...updating .CSS, .JS, and .html...

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
-//----------------------   GLOBAL VARIABLE DECLARATION   ----------------------//
+//----------------------   CODE BEGINS HERE   ----------------------//
 
+//----------------------   GLOBAL VARIABLE DECLARATION   ----------------------//
 //1- Defining an array whose elements are objects. Each object represent a question, multiple choices, and the correct answer associated with the question!
 var qArr = [{
         q: "What is the capital of The United States of America?",
@@ -15,18 +16,32 @@ var qArr = [{
         q: "What is the largest country in the world?",
         choices: ["United States", "Canada", "China", "Russia"],
         correctAns: "Russia"
+    },
+    {
+        q: "Who has been a president of the United States of America?",
+        choices: ["Michael Jackson", "Maddona", "John F. Kennedy", "Vladimir Putin"],
+        correctAns: "John F. Kennedy"
+    },
+    {
+        q: "Which of the following activity is most joyful?",
+        choices: ["Skiing", "Watching Movies", "Sunbathing", "Coding"],
+        correctAns: "Coding"
+    },
+    {
+        q: "Will Google exist 100 years from now?",
+        choices: ["Yes", "No", "Maybe", "Don't know!"],
+        correctAns: "Maybe"
     }
 ]; // End of qArr
 // 2- Getting a reference to the main row
 var row = $(".row");
 // 3- A variable to question's number
 var qNum = 0;
-// 4- Creating a reference to the sumbit button
-var submitBtn = $('<button type="button" id="submit-btn">Sumbit</button>');
+// 4- Creating sumbit button object 
+var submitBtn = $('<button type="button" class="btn" id="submit-btn">Sumbit</button>');
 
 
-//----------------------   DISPLAY QUIZ FUNCTION   ----------------------//
-
+//----------------------   FUNCTION DEFINITION TO SHOW QUIZ   ----------------------//
 function displayQuiz() {
     // 1-2) Adding questions and choices 
     // Looping through qArr and adding questions and choices into the page 
@@ -51,10 +66,9 @@ function displayQuiz() {
         row.append(newDiv);
     }) // End of forEach() method
 
-    // 2-2) Adding a sumbit button to the bottom of the page 
-    //submitBtn = $('<button type="button" id="submit-btn">Sumbit</button>');
+    // 2-2) Adding the sumbit button to the bottom of the page 
     row.append(submitBtn);
-} // End of displayQuiz definition 
+} // End of displayQuiz() definition 
 
 
 //----------------------   EVENT HANDLER FOR THE SUBMIT BUTTON   ----------------------//
@@ -91,8 +105,7 @@ $(document).ready(function () {
         console.log("correct: " + numOfCorrectAns);
         console.log("wrong: " + numOfWrongAns);
 
-
-
-
     }); // End of button's event listener
 }); // End of document.ready()
+
+//----------------------   CODE ENDS HERE   ----------------------//
