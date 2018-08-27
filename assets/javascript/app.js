62e6baf63b7b2bb3831cc6271521f24f4186ecc3
My Git Arsalan <asadri62@gmail.com>
2018-08-27 02:19:06 -0700

    ...updating...

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,18 @@
 //----------------------   DOCUMENT BEGINS HERE   ----------------------//
-// STRUCTURE OF THIS DOCUMENT:
-// GLOBAL VARIABLES DECLARATION
-// FUNCTION TO DISPLAY THE QUIZ 
-// FUNCTION TO EXTRACT USER'S ANSWERS AND MAKE COMPARISION
-// FUNCTION TO DISPLAY THE RESULT
-// EVENT HANDLER FOR THE START BUTTON
-// EVENT HANDLER FOR THE SUBMIT BUTTON
+// CONTENT:
+// 1-8) GLOBAL VARIABLES DECLARATION
+// 2-8) FUNCTION TO DISPLAY THE TIMER
+// 3-8) FUNCTION TO DISPLAY THE QUESTIONS
+// 4-8) FUNCTION TO DISPLAY THE QUIZ 
+// 5-8) FUNCTION TO GRADE THE QUIZ OPTION 1 AND OPTION 2
+// 6-8) FUNCTION TO DISPLAY THE RESULT
+// 7-8) EVENT HANDLER FOR THE START BUTTON
+// 8-8) EVENT HANDLER FOR THE SUBMIT BUTTON
 
 //----------------------   GLOBAL VARIABLES DECLARATION   ----------------------//
 // 1- Defining an array whose elements are objects. 
 // Each object represent a question, multiple choices, and the correct answer associated with the question!
-var qArr = [{
+var questionArr = [{
         q: "What is the capital of The United States of America?",
         choices: ["New York", "Washington", "Washington DC", "Chicago"],
         correctAns: "Washington DC"
@@ -40,73 +42,144 @@ var qArr = [{
         choices: ["Yes", "No", "Maybe", "Don't know!"],
         correctAns: "Maybe"
     }
-]; // End of qArr
+]; // End of questionArr
 // 2- Getting a reference to the main row
 var row = $(".row");
 // 3- Variable to hold the number of the current question
-var qNum = 0;
+var questionNum = 0;
 // 4- Creating sumbit button object 
 var submitBtn = $('<button type="button" class="btn" id="submit-btn">Sumbit</button>');
-// 5- Variables to count the nubmer of right and wrong answer
+// 5- Variables to count the nubmer of correct answeres
 var numOfCorrectAns = 0;
 // 6- Variables to count the nubmer of wrong answers
 var numOfWrongAns = 0;
+// 6- Variables to count the nubmer of unanswered questions
+var numOfUnAnswered = 0;
+// 7- Variables for timer's interval
+var intervalID;
+//----------------------   FUNCTION TO DISPLAY THE TIMER   ----------------------//
+function displayTimer() {
+    // 
+    $(".timerDiv").append($('<h2>Time remaining: <span class="timer"></span> seconds</h2>'));
+    intervalID = setInterval(myTimer, 1000);
+    // Setting the counter to 60 seconds 
+    counter = 60;
 
-//----------------------   FUNCTION TO DISPLAY THE QUIZ   ----------------------//
-function displayQuiz() {
-    // 1-2) Adding questions and choices 
-    // Looping through qArr and adding questions and choices into the page 
-    qArr.forEach(function (element) {
-        // Making a new <div> object to accommodate the question and choices
+    function myTimer() {
+        $(".timer").text(counter);
+        if (counter === 0) {
+            // Grade quiz
+            gradeQuiz2();
+            // Display the result
+            displayResult();
+        }
+        counter--;
+    }
+} // End of displayTimer
+
+//----------------------   FUNCTION TO DISPLAY THE QUESTIONS   ----------------------//
+function displayQuestions() {
+    // Looping through questionArr and adding questions and choices into the page 
+    questionArr.forEach(function (element) {
+        // Making a new <div> to dynamically add questions and choices
         var newDiv = $("<div>");
         // Building the question html string
         var qStr = "<h3>" + element.q + "</h3>";
         // Increment the number of questions by 1
-        qNum++;
+        questionNum++;
         // Adding the question to the new <div>
         newDiv.append(qStr);
 
         // Looping through choices array and retrieving one by one 
         for (i = 0; i < element.choices.length; i++) {
             // Constructing an radio <input> element for each choice 
-            var choiceStr = '<input type="radio" name="' + qNum + '" value="' + element.choices[i] + '">' + element.choices[i];
+            var choiceStr = '<input type="radio" name="' + questionNum + '" value="' + element.choices[i] + '">' + element.choices[i];
             // Adding the choices to the new <div>
             newDiv.append(choiceStr);
         } // End of for loop to add radio inputs for choices
         // Adding the <div> to the main row
         row.append(newDiv);
     }) // End of forEach() method
-    // 2-2) Adding the sumbit button to the bottom of the page 
+} // End of displayQuestions()
+
+//----------------------   FUNCTION TO DISPLAY THE QUIZ   ----------------------//
+function displayQuiz() {
+    // 1/4) Empty the content 
+    $(".row").empty();
+    // 2/4) Display the timer
+    displayTimer();
+    // 3/4) Display the questions
+    displayQuestions();
+    // 4/4) Add submit button 
     row.append(submitBtn);
 } // End of displayQuiz() definition 
 
-//----------------------   FUNCTION TO EXTRACT USER'S ANSWERS AND MAKE COMPARISION   ----------------------//
-function retrieveAndCompare() {
+//----------------------   FUNCTION TO GRADE THE QUIZ OPTION 1   ----------------------//
+function gradeQuiz1() {
     // The number of current question sets back to 1
-    qNum = 1;
+    questionNum = 1;
     // Start looping back through the array of questions, comparing user's ansewr with the correnct answer
-    qArr.forEach(function (element) {
-        // Starting from top of the form, extracting user's answer from each question one bye one 
-        var userAns = $('input[name="' + qNum + '"]:checked').val();
+    questionArr.forEach(function (element) {
+        // Starting from question 1, get a jquery object of all choices  
+        var choices = $('input[name="' + questionNum + '"]');
+        // Starting from question 1, get a jquery object of only the choice that has been checked, if any
+        var checkedChoice = $('input[name="' + questionNum + '"]:checked');
+        // Determine if user has answered this question  
+        if ((choices.is(checkedChoice))) {
+            var userAns = checkedChoice.val();
+            if (userAns === element.correctAns) {
+                numOfCorrectAns++;
+            }
+            if (userAns !== element.correctAns) {
+                numOfWrongAns++;
+            }
+        }
+        // If user has not answered at all 
+        else {
+            numOfUnAnswered++;
+        };
         // next question (making it read)y for the next loop)
-        qNum++;
-        // Having retrieved the user's answer, compare it against the correct answer right away
-        if (userAns === element.correctAns) {
-            numOfCorrectAns++;
-        }
-        if (userAns !== element.correctAns) {
-            numOfWrongAns++;
-        }
+        questionNum++;
     }); // End of forEach()
-} // End of function retrieveAndCompare()
+} // End of function gradeQuiz1()
+
+//----------------------   FUNCTION TO GRADE THE QUIZ OPTION 2   ----------------------//
+function gradeQuiz2() {
+    // The number of current question sets back to 1
+    questionNum = 1;
+    // Start looping back through the array of questions, comparing user's ansewr with the correnct answer
+    questionArr.forEach(function (element) {
+        // Find the checked radio input and get its value
+        var userAns = $('input[name="' + questionNum + '"]:checked').val();
+        // if user has not answered at all
+        if (userAns === undefined) {
+            numOfUnAnswered++;
+            // if user answered correctly
+        } else if (userAns === element.correctAns) {
+            numOfCorrectAns++;
+            console.log("correct");
+        }
+        // if user answered wrongly
+        else if (userAns !== element.correctAns) {
+            numOfWrongAns++;
+            console.log("wrong");
+        }
+        questionNum++;
+    }); // End of forEach()
+} // End of function gradeQuiz2()
 
 //----------------------   FUNCTION TO DISPLAY THE RESULT   ----------------------//
 function displayResult() {
+    // Kill the timer
+    clearInterval(intervalID);
+    // Show "time's up" msg
+    $(".timerDiv").html("<h2>Time's Up!</h2>");
     // empty the row 
     $(".row").empty();
     // Adding to the row
     row.append("<h3>The number of correct answers are: " + numOfCorrectAns + "</h3>");
     row.append("<h3>The number of wrong answers are: " + numOfWrongAns + "</h3>");
+    row.append("<h3>The number of unanswered questions are: " + numOfUnAnswered + "</h3>");
 } // End of displayResult() function
 
 // Wrapping both event handlers inside ready()
@@ -116,17 +189,14 @@ $(document).ready(function () {
     // 1- Event handler for the start button, which basically calls the display quiz function
     $("#start-btn").on("click", function () {
         // Display questions 
-        $(".row").empty();
         displayQuiz();
-        // Display the timer
-
     }); // End of start button event handler
 
     //----------------------   EVENT HANDLER FOR THE SUBMIT BUTTON   ----------------------//
     // Event handler for the submmit button 
     submitBtn.on("click", function () {
         // Get the user's answer and compare it against the coorect anwer 
-        retrieveAndCompare();
+        gradeQuiz2();
         // Display the result
         displayResult();
     }); // End of button's event listener
